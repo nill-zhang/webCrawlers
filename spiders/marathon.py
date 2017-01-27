86326727864a46b51a20e86d4de3d2ca13964733
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-01-26 20:37:38 -0500

    fixing different years result has different column format

--- spiders/marathon.py
+++ spiders/marathon.py
@@ -17,6 +17,7 @@ id_mapping = {"boston": "15160418",
 class MarathonSpider(Spider):
     name = "marathon"
     allowed_domains = ["marathonguide.com"]
+    has_header = False
 
     def start_requests(self):
         base_url = "http://www.marathonguide.com/results/browse.cfm?MIDD="
@@ -27,20 +28,21 @@ class MarathonSpider(Spider):
             urls = [""]
 
         for url in urls:
-            url = urljoin(base_url, url.replace(".."))
+            url = urljoin(base_url, url.strip(".."))
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        year_urls = response.xpath(
-            "//b[contains(text(),'Marathon Results') \
-            and string-length(text()) >20]/following-sibling::a/@href") \
-            .extract()
+        year_urls = response.xpath("//b[contains(text(),'Marathon Results') and "
+                                   "string-length(text()) >20]/following-sibling::a/@href")\
+                                    .extract()
         for url in year_urls:
+            url = response.urljoin(url)
             yield scrapy.Request(url=url, callback=self.parse_one_year)
 
     def parse_one_year(self, response):
         """parse one specific year and yield all the section pages result"""
         # sections = response.css("form")[0].css("select")[0].css("option::attr(value)")[1:].extract()
+        self.has_header = False
         sections = response.xpath("//form/select/option/@value").extract()[1:]
         for section in sections:
             form_data = {'RaceRange': section}
@@ -52,14 +54,16 @@ class MarathonSpider(Spider):
 
     def parse_one_section(self, response):
         """parse one section and yield entries in that section"""
-        # if not self.has_header:
-        #     csv_header = list(map(lambda x: re.sub(r"<.+?>", "", x).strip(), response.css("th").extract()))
-        #     self.has_header = True
-        #     yield dict(enumerate(csv_header))
+        if not self.has_header:
+        # csv_header = list(map(lambda x: re.sub(r"<.+?>", "", x).strip(), response.css("th").extract()))
+        # self.has_header = True
+        # yield dict(enumerate(csv_header))
         # raw_entries = response.css("table")[5].css("tr")[7:]
         # for entry in raw_entries:
         #     yield dict(enumerate(entry.css("td::text").extract()))
-        column_name = map(lambda x: re.sub(r"<.+?>", "", x).strip(), response.css("th").extract())
+            csv_header = list(map(lambda x: re.sub(r"<.+?>", "", x).strip(), response.css("th").extract()))
+            yield dict(enumerate(csv_header))
+            self.has_header = True
         raw_entries = response.xpath("//tr[th]/following-sibling::tr")
         for entry in raw_entries:
-            yield dict(zip(column_name, entry.xpath("td/text()").extract()))
+            yield dict(enumerate(entry.xpath("td/text()").extract()))
